085196376bacd29de45e878da44eabf6c98dafeb
iago <iagogoliver@gmail.com>
2025-02-12 18:44:53 -0300

    Initialized migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using urlShortener.Models;
+
+namespace urlShortener.Data
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Address> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=urlShortener;Trusted_Connection=True;TrustServerCertificate=True;",
+                            options => options.MigrationsHistoryTable("__MigrationsHistory_urlShortener"));
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace urlShortener.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string NewUrl { get; set; } = string.Empty;
+        public int UserId { get; set; }
+
+    }
+}
